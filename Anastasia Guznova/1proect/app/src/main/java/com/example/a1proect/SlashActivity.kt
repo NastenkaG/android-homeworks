1b496fa4bc02a9370f3572aaf2f7c552904427dd
guzno <anastasia.guznova@yandex.ru>
2021-11-01 14:58:57 +0300

    correction of new comments

--- Anastasia Guznova/1proect/app/src/main/java/com/example/a1proect/SlashActivity.kt
+++ Anastasia Guznova/1proect/app/src/main/java/com/example/a1proect/SlashActivity.kt
@@ -1,7 +1,7 @@
 package com.example.a1proect
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.example.a1proect.databinding.ActivitySlashBinding
 
 class SlashActivity : AppCompatActivity() {
