2356ad14af04fad275b79111e0828639eb7139ac
guzno <anastasia.guznova@yandex.ru>
2021-11-01 15:00:54 +0300

    correction of new comments

--- Anastasia Guznova/1proect/app/src/main/java/com/example/a1proect/MainActivity.kt
+++ Anastasia Guznova/1proect/app/src/main/java/com/example/a1proect/MainActivity.kt
@@ -1,6 +1,6 @@
 package com.example.a1proect
-import android.os.Bundle
 import android.content.Intent
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.a1proect.databinding.ActivityMainBinding
 
