4169de3e7aab3f3fe6ad55231e8b8b75ac8adb5f
Kosov Andrey <92647577+KosovAndrey@users.noreply.github.com>
2022-05-19 13:58:48 +0300

    Add Kosov-L5

--- Andrey Kosov/Andrey Kosov/AndreyKosov/app/src/main/java/com/example/andreykosov/fragments/TasksFragment.kt
+++ Andrey Kosov/Andrey Kosov/AndreyKosov/app/src/main/java/com/example/andreykosov/fragments/TasksFragment.kt
@@ -5,7 +5,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.andreykosov.MyPagerAdapter
+import com.example.andreykosov.R
 import com.example.andreykosov.databinding.FragmentTasksBinding
+import com.google.android.material.tabs.TabLayoutMediator
 
 class TasksFragment : Fragment() {
     private lateinit var binding: FragmentTasksBinding
@@ -15,6 +18,15 @@ class TasksFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentTasksBinding.inflate(inflater, container, false)
+        binding.viewPager?.adapter = MyPagerAdapter(this)
+        TabLayoutMediator(binding.tabLayout, binding.viewPager) { tab, position ->
+            tab.text = when (position) {
+                0 -> getString(R.string.task_work)
+                1 -> getString(R.string.task_entertainment)
+                2 -> getString(R.string.task_study)
+                else -> null
+            }
+        }.attach()
         return binding.root
     }
 }
