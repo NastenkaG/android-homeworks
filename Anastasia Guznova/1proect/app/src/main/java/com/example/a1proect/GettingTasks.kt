f8624cb09fdeb8cde8fe09e70e4df3b6f2de906e
guzno <anastasia.guznova@yandex.ru>
2022-05-27 13:31:33 +0300

    task 10 ((((((

--- Anastasia Guznova/1proect/app/src/main/java/com/example/a1proect/GettingTasks.kt
+++ Anastasia Guznova/1proect/app/src/main/java/com/example/a1proect/GettingTasks.kt
@@ -4,16 +4,16 @@ import com.google.gson.annotations.SerializedName
 
 data class GettingTasks(
 
-@field:SerializedName("id")
-val id: Int,
+    @field:SerializedName("id")
+    val id: Int,
 
-@field:SerializedName("text")
-val text: String,
+    @field:SerializedName("text")
+    val text: String,
 
-@field:SerializedName("isСompleted")
-val isCompleted: Boolean,
+    @field:SerializedName("isСompleted")
+    val isCompleted: Boolean,
 
-@field:SerializedName("userId")
-val userId: Int
+    @field:SerializedName("userId")
+    val userId: Int
 
 )
